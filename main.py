35fdfb489c3d0de002cf590b35291b6ed18eda60
SamVeiga <veigafotos3@gmail.com>
2025-07-29 06:25:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 # =======================================
 # CONFIGURAÇÕES INICIAIS
 # =======================================
-TOKEN = os.getenv("BOT_TOKEN") or "{"ok":true,"result":true,"description":"O webhook já está definido"}"
+TOKEN = os.getenv("BOT_TOKEN") or "{"7646672843:AAHckKPRXKDbEwbRGfY7KTtQEtw27jrQl_U"
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
