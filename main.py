36265e2e6485fa0081722be557d79a235a68586a
SamVeiga <veigafotos3@gmail.com>
2025-08-10 14:09:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def callback(call):
     comandos = {
         "show": "/quiz",
         "forca": "/forca",
-        "velha": "@xoBot play",
+        "velha": "@xoBot",
         "uno": "/new@unopybot",
         "bobbie_goods": "/start@DoodleGatorBot"  # Novo jogo
     }
